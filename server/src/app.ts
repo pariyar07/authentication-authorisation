7b0b7b3ce5925e040d389b15938b61a49884bacb
Satyam Pariyar <88579174+pariyar07@users.noreply.github.com>
2024-06-02 17:50:19 +0530

    MongoDb setup with mongoose

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,12 @@
+import connectDB from './config/db.config';
 // import routes from './routes';
 
 import express from 'express';
 const app = express();
 
 app.use(express.json());
+
+connectDB();
 
 // app.use();
 
